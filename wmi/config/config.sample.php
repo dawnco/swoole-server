a862619364ff645c0706741a3909cf0b97389ba8
root <root@localhost.localdomain>
2019-07-30 20:07:14 +0800

    config

--- wmi/config/config.sample.php
+++ wmi/config/config.sample.php
@@ -5,21 +5,23 @@
  */
 
 return [
-    "app"   => [
-        'host' => "0.0.0.0",
-        "port" => "9999",
+    "worker_num" => 5,
+    "app"        => [
+        'host'      => "0.0.0.0",
+        "port"      => "9999",
+        'log_level' => 'debug',
     ],
-    "redis" => [
+    "redis"      => [
         'host'     => "127.0.0.1",
         "port"     => "6379",
         "database" => 0,
         "pool"     => [
-            "max" => 20,
-            "min" => 10,
+            "max" => 4,
+            "min" => 2,
         ]
     ],
-    "mysql" => [
-        'host'     => "127.0.0.1",
+    "mysql"      => [
+        'host'     => "192.168.0.11",
         "port"     => "3306",
         "user"     => "root",
         "database" => "redpacket",
@@ -27,8 +29,8 @@ return [
         "timeout"  => 10,
         'charset'  => 'utf8mb4',
         "pool"     => [
-            "max" => 20,
-            "min" => 10,
+            "max" => 4,
+            "min" => 2,
         ]
     ],
 ];
